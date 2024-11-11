7bf6ede545cff4390d5be795f9fcb814e98261ab
mathfrancisco2 <math.francisco2@gmail.com>
2024-11-11 12:29:44 -0300

    att

--- nexus-front/nexus-front/src/app/app.component.ts
+++ nexus-front/nexus-front/src/app/app.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './shared/components/navbar/navbar.component';
-import { FooterComponent } from './shared/components/footer/footer.component';
+import {FooterComponent} from './shared/components/layout/footer/footer.component';
+import {NavbarComponent} from './shared/components/layout/navbar/navbar.component';
+import {RouterOutlet} from '@angular/router';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, NavbarComponent, FooterComponent],
   template: `
-
+    <app-navbar></app-navbar>
+    <main class="min-h-screen">
+      <router-outlet></router-outlet>
+    </main>
+    <app-footer></app-footer>
   `
 })
 export class AppComponent {}
